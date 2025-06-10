45e5c1d988663f54e8306b1038e6105b24b458dc
Abdul Kabeer <abdulkabeer2143@gmail.com>
2025-06-11 01:24:51 +0500

    Update website_assistant.py

--- website_assistant.py
+++ website_assistant.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://zonixo.com/"],
+    allow_origins=["https://zonixo.com/","https://ai-assistant-8x6e.onrender.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
